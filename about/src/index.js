78ddf675fe55b014c8925795adaecd519ef01e41
zepeng cai <caizepeng@coding.net>
2021-07-09 17:46:13 +0800

    feat: use react-single-react

--- about/src/index.js
+++ about/src/index.js
@@ -1,24 +1,17 @@
 import React from "react";
-import ReactDOM, { unmountComponentAtNode }  from "react-dom";
-import App from './App';
 
-export const bootstrap = async () => {
+import React from "react";
+import ReactDOM from "react-dom";
+import singleSpaReact from "single-spa-react";
+import App from "./App";
 
-}
-export const mount = async () => {
-  ReactDOM.render(
-    <div>
-      <h1>About</h1>
-      <App/>
-    </div>,
-    document.getElementById('container'),
-  );
+const reactLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  domElementGetter: () => document.getElementById("container"),
+  rootComponent: App,
+});
 
-}
-export const unmount = async () => {
-  const e = document.getElementById('container')
-  if (e) {
-    unmountComponentAtNode(e)
-  }
-  
-}
+export const bootstrap = reactLifecycles.bootstrap;
+export const mount = reactLifecycles.mount;
+export const unmount = reactLifecycles.unmount;
